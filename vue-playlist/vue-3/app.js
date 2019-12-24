775f355c773b5f1a4cf4038cd95559c842217df2
liyiruo <401213245@qq.com>
2019-12-25 00:27:58 +0800

    vue-3

---
+++ vue-playlist/vue-3/app.js
@@ -0,0 +1,35 @@
+new Vue({
+	el:"#vue-app",
+	data:{
+		age:30,
+		x:0,
+		y:0
+	},
+	methods:{
+		add:function(inc){
+		this.age+=inc;
+		},
+		sub:function(des){
+		this.age-=des;
+		},
+		updateXY:function(event){
+		console.info(event);
+
+		this.x=event.offsetX;
+		this.y=event.offsetY;
+		},
+
+		stopMoving:function(event){
+			event.stopPropagation();
+		},
+
+		alert:function(){
+			alert("hello world");
+		}
+		
+	}
+})
+/**
+
+*/
+
