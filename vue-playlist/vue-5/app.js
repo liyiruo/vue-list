dfbcbfe3a12e4c4dcd810b05b1a65d1fc53a8ead
liyiruo <401213245@qq.com>
2019-12-25 21:13:03 +0800

    Events事件

---
+++ vue-playlist/vue-5/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el:"#vue-app",
+	data:{
+		
+	},
+
+	methods:{
+		logName: function(){
+console.info("你正在输入你的名字");
+		},
+		logAge: function(){
+console.info("你正在输入你的年龄");
+		}
+}
+})
+
+
